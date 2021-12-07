76c1a0cc45edbd8f2e2fbaa3a7bd26ca8256d275
Harry <harryguloien@gmail.com>
2021-12-07 15:50:36 -0500

    send and grab media comp[lete

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,10 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // const cbApp = initializeApp(firebaseConfig);
 console.log('connected')
+const imagePlaceholderURl = 'https://firebasestorage.googleapis.com/v0/b/cbsays-6bb73.appspot.com/o/cbMedia%2Fdisneylandplaceholder.jpg?alt=media&token=a775b233-096a-4fa3-88c2-5021db30861a';
+
+cbApp.currentUploadUrl = '';
+cbApp.mediaList = [];
 // Import the functions you need from the SDKs you need
 
 // cbApp.auth = getAuth();
@@ -39,18 +43,115 @@ console.log('connected')
 //     const errorCode = error.code;
 //     const errorMessage = error.message;
 //   });
+cbApp.getFileType = function(file) {
+  let fileName, fileExtension;
+  fileName = file;
+  fileExtension = fileName.replace(/^.*\./, '');
+  return fileExtension;
+}
 
 cbApp.storage = firebase.storage();
 cbApp.storageRef = cbApp.storage.ref();
+cbApp.cbMedia = cbApp.storageRef.child('cbMedia');
+console.log(cbApp.cbMedia)
 
-cbApp.addNewMedia = function() {
-  // Write function for this now
-  console.log('I dont wsnt these errors')
+
+cbApp.addNewMedia = function(file) {
+  const mediaType = file.type;
+  const mediaName = file.name;
+  console.log(file, file.name)
+  const metadata = {
+    contentType: mediaType
+  }
+
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  const uploadTask = cbApp.storageRef.child('cbMedia/' + file.name).put(file, metadata);
+
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    },
+    (error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    },
+    () => {
+      // Upload completed successfully, now we can get the download URL
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log('File available at', downloadURL);
+        cbApp.currentUploadUrl = downloadURL;
+        cbApp.mediaList.push(downloadURL)
+      });
+    }
+  );
+
+
+
 }
 
-cbApp.grabMedia = function() {
-  // write function for this to
-  console.log('I dont wsnt these errors')
+cbApp.grabAndSetMedia = function (fileName, folder) {
+ let mediaRef; 
+
+ // Create a reference to the file we want to download
+  folder ? mediaRef = cbApp.storageRef.child(folder + '/' + fileName)
+    : mediaRef = cbApp.storageRef.child('cbMedia/' + fileName);
+  // Get the download URL
+  mediaRef.getDownloadURL()
+    .then((url) => {
+      console.log(url.toString())
+      const urlString = url.toString();
+    //  setTimeout(function(){
+       urlString.includes('jpg') || urlString.includes('jpeg') || urlString.includes('png')
+         ? $('#picture-frame').html('<img src="' + url + '" alt="Disneyland castle lit up for the holidays">')
+         : $('#picture-frame').html('<iframe id="cardVideo-html" src="' + url + '" frameborder="0" scrolling="no" webkitallowfullscreen="" mozallowfullscreen = "" allowfullscreen = "" allow = "autoplay; fullscreen"style = "position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;" ></iframe >')
+    //  }, 2000) 
+    })
+    .catch((error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
 }
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
@@ -97,12 +198,20 @@ cbApp.cardCreator = function (title, subtitle, video, transcript, message) {
   $('#cardTitle-html').text(title);
   subtitle ? $('#cardSubtitle-html').text(subtitle)
     : $('#cardSubtitle-html').hide();
-  video ? $('#cardVideo-html').attr('src', video)
-    : $('#picture-frame').html('<img src="assets/media/disneylandplaceholder.jpg" alt="Disneyland castle lit up for the holidays">');
+
   transcript ? cbApp.paragraphBuilder(transcript, 'cardTranscript-html')
     : $('#cardTranscript').hide();
   message ? cbApp.paragraphBuilder(message, 'cardMessage-html')
     : $('#cardMessage').hide();
+
+        console.log('hello time')
+        if (video) {
+          console.log(video)
+          cbApp.grabAndSetMedia(video)
+        } else {
+          $('#picture-frame').html('<img src="' + imagePlaceholderURl + '" alt="Disneyland castle lit up for the holidays">');
+        }
+     
 }
 
 cbApp.authenticateUser = function() {
@@ -129,15 +238,35 @@ cbApp.authenticateUser = function() {
 
 
 cbApp.createNewQrCode = function() {
+  let mediaToLoad = {};
+  // capture file on upload
+  $('#media-input').on('change', function () {
+    console.log(this.files);
+    mediaToLoad = {
+      file: this.files[0],
+      name: this.files[0].name,
+      type: this.files[0].type
+    }
+  })
 
   $('#card-form').on('submit', function (e) {
     e.preventDefault();
     $('#qr').empty();
     let urlParams = '';
     let paramsArray = [];
-    console.log($(this).find(':input'));
-    const inputArray = $(this).find(':input.user-input');
 
+    if(mediaToLoad != {}){
+      const mediaObject = {
+        'media-input': mediaToLoad.name
+      }
+      cbApp.allInputs.push(mediaObject);
+      cbApp.addNewMedia(mediaToLoad.file)
+
+    }
+
+    console.log($(this).find(':input'));
+    const inputArray = $(this).find(':input.user-input:not(#media-input)');
+   
     for (let input in inputArray) {
       const inputId = inputArray[input].id;
       const inputValue = inputArray[input].value;
@@ -155,7 +284,7 @@ cbApp.createNewQrCode = function() {
       const queryString = cbApp.encodeQueryData(input);
       paramsArray.push(queryString)
     })
-
+  
     urlParams = "?" + paramsArray.join('&');
 
     // const mySite = window.location.origin;
@@ -191,7 +320,7 @@ cbApp.initiateCardPage = function () {
     cardUrl = cbApp.getUrlParameter('url-input'),
     cardTitle = cbApp.getUrlParameter('title-input'),
     cardSubtitle = cbApp.getUrlParameter('subtitle-input'),
-    cardVideo = cbApp.getUrlParameter('video-input'),
+    cardVideo = cbApp.getUrlParameter('media-input'),
     cardTranscript = cbApp.getUrlParameter('transcript-text-input'),
     cardMessage = cbApp.getUrlParameter('message-input');
 
