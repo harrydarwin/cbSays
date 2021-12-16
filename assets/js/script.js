4327f0bc86e09b9799a7f0107e7a2d00f23deeb6
Harry <harryguloien@gmail.com>
2021-12-16 17:53:42 -0500

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,8 +137,7 @@ cbApp.addNewMedia = function(file) {
 cbApp.grabAndSetMedia = function (fileName, folder) {
   if (fileName === "CeciliaBrush_MC.mp4"){
     videojs('my_video_1', {
-      poster: "assets/media/disneylandplaceholder.jpg",
-      fluid: "true"
+      poster: "assets/media/disneylandplaceholder.jpg"
     });
   } else {
     
@@ -165,10 +164,7 @@ cbApp.grabAndSetMedia = function (fileName, folder) {
           //  .html('<iframe class="wrapped-iframe cardVideo-html" src="' + url + '" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
           //  $('#picture-frame').html('<iframe id="cardVideo-html" src="' + url + '" frameborder="0" scrolling="no" webkitallowfullscreen="" mozallowfullscreen = "" allowfullscreen = "" allow = "autoplay; fullscreen"style = "position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;" ></iframe >')
       //  }, 2000) 
-           videojs('my_video_1', {
-            //  poster: "assets/media/disneylandplaceholder.jpg",
-             fluid: "true"
-           });
+           videojs('my_video_1');
       })
       .catch((error) => {
         // A full list of error codes is available at
